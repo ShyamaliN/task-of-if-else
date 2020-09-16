1cea09ccbf0d447457aeaae00613e1def6f3499e
ShyamaliN <71086088+ShyamaliN@users.noreply.github.com>
2020-09-16 18:50:35 +0530

    Add files via upload

---
+++ mycapvote.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std ;
+int main()
+{  
+    // to check whether the candidate is eligible to vote or not
+	int  age;
+	cout<<"\nEnter current age of the candidate :";
+	cin>>age;
+	if(age>=18)
+	{
+	
+	cout<<"\nthe candidate is eligible to vote";
+	
+     }
+else 
+     {
+	 
+	cout<<"\nthe candidate is not eligible to vote";
+	
+     }
+}
